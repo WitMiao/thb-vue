a52c5e3cc5a52a5cc4bbf16b87c8e581d25afeec
WitMiao <454690789@qq.com>
2021-07-10 16:32:57 +0800

    toTop按钮

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,19 @@
       <router-view/>
     </v-main>
     <Footer/>
+    <ToTop/>
   </v-app>
 </template>
 
 <script>
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import ToTop from "@/components/ToTop";
 
 export default {
   name: 'App',
   components: {
+    ToTop,
     Header,
     Footer,
   },
---
+++ src/components/ToTop/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-btn
+      v-scroll="onScroll"
+      v-show="btnShow"
+      fab
+      dark
+      fixed
+      bottom
+      right
+      color="orange"
+      @click="toTop"
+  >
+    <v-icon large>mdi-chevron-up</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'ToTop',
+  data() {
+    return {
+      btnShow: false
+    }
+  },
+  methods:{
+    onScroll (e) {
+      if (typeof window === 'undefined') return
+      const top = window.pageYOffset ||   e.target.scrollTop || 0
+      this.btnShow = top > 20
+    },
+    toTop () {
+      this.$vuetify.goTo(0)
+    }
+  }
+};
+</script>
+
+<style></style>
